7ab245d385599a786add66925b1cb0b176d58a5c
mukitul <mukitulislamratul@gmail.com>
2019-12-06 21:11:08 +0600

    basic array/stack operation

---
+++ 5-ds-objects-arrays.js
@@ -0,0 +1,15 @@
+// array
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log("value at index 0 ",listOfNumbers[0]);
+console.log("value at index 6 ",listOfNumbers[6]);
+console.log("length/size of array: ", listOfNumbers.length);
+
+
+// stack
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+
+console.log(sequence);
+console.log(sequence.pop());
+console.log(sequence);
